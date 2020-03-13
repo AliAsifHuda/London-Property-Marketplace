a155a158f5ad3656454d0503a1d85068e49d80ad
Ali Asif <k19033243@kcl.ac.uk>
2020-03-13 21:27:32 +0000

    REFACTORING. Used better lambdas and encapsulation.

--- Main.java
+++ Main.java
@@ -22,7 +22,6 @@ import javafx.event.EventHandler;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
-
 import javafx.geometry.Bounds;
 import javafx.geometry.Pos;
 import javafx.stage.Popup;
@@ -39,6 +38,15 @@ public class Main extends Application
 {
     private Stage window;
     private Scene scene1;
+    private BorderPane borderPane = new BorderPane();
+    private StackPane textPane = new StackPane();
+    private ImageView imageView;
+    private StackPane stackPane = new StackPane();  
+    private ComboBox<Integer> fromComboBox = new ComboBox<>();
+    private ComboBox<Integer> toComboBox = new ComboBox<>();
+    private Button backButton = new Button("Back");
+    private Button forwardButton = new Button("Forward");
+    private Text text1 = new Text("Welcome to London Property Marketplace ");
     private int counter;
     private boolean disableFlagA = false;
     private boolean disableFlagB = false;
@@ -59,46 +67,35 @@ public class Main extends Application
     public final void start(Stage window) {
         // create the buttons needed for boroughs
         createBoroughButtons();
-        // window = window;
         window.setTitle("Property");
 
         //controls and lines for the top bit
         HBox commonLayout = new HBox(15);
-        commonLayout.setStyle("-fx-border-color: black; -fx-border-width: 0px 0px 1px 0px; -fx-padding: 5px;");
         Label fromLabel = new Label("From");
         Label backLabel = new Label("To");
-        ComboBox<Integer> fromComboBox1 = new ComboBox<>();
-        fromComboBox1.setPromptText("min"); 
-        addComboBoxRanges(fromComboBox1, dataLoader.getMinPriceListing().getPrice(), dataLoader.getMaxPriceListing().getPrice());
-        ComboBox<Integer> toComboBox1 = new ComboBox<>();
-        toComboBox1.setPromptText("max"); 
-        addComboBoxRanges(toComboBox1, dataLoader.getMinPriceListing().getPrice(), dataLoader.getMaxPriceListing().getPrice());
-        AnchorPane fromToButtonPane = new AnchorPane();
+        fromComboBox.setPromptText("min"); 
+        addComboBoxRanges(fromComboBox, dataLoader.getMinPriceListing().getPrice(), dataLoader.getMaxPriceListing().getPrice());
+        toComboBox.setPromptText("max"); 
+        addComboBoxRanges(toComboBox, dataLoader.getMinPriceListing().getPrice(), dataLoader.getMaxPriceListing().getPrice());
+        AnchorPane fromToButtonPane = new AnchorPane();//AnchorPane to align the buttons
         commonLayout.setHgrow(fromToButtonPane , Priority.ALWAYS); 
         commonLayout.getChildren().add(fromToButtonPane);
-        //add all control items to layout style
-        commonLayout.getChildren().addAll(fromLabel, fromComboBox1, backLabel, toComboBox1);
+        commonLayout.getChildren().addAll(fromLabel, fromComboBox, backLabel, toComboBox);
 
         //text for the middle
-        Text text1 = new Text("Welcome to London Property Marketplace ");
-        StackPane textPane = new StackPane();
         textPane.getChildren().addAll(text1);
 
         //controls and lines for the bottom bit
         HBox bottomBox = new HBox(15);
-        bottomBox.setStyle("-fx-border-color: black; -fx-border-width: 1px 0px 0px 0px; -fx-padding: 5px ;");
-        Button backButton = new Button("Back");
-        Button buttonForward = new Button("Forward");
-        AnchorPane backButtonPane = new AnchorPane();
-        AnchorPane forwardButtonPane = new AnchorPane();
+        AnchorPane backButtonPane = new AnchorPane();//AnchorPane to align the buttons
+        AnchorPane forwardButtonPane = new AnchorPane();//AnchorPane to align the buttons
         HBox.setHgrow(backButtonPane , Priority.ALWAYS);//Make pane grow horizontally
         bottomBox.getChildren().add(backButtonPane );
         bottomBox.getChildren().add(forwardButtonPane );
         backButtonPane.getChildren().add(backButton);
-        forwardButtonPane.getChildren().add(buttonForward);
+        forwardButtonPane.getChildren().add(forwardButton);
 
         //make a borderPane layout and add layouts to top and bottom
-        BorderPane borderPane = new BorderPane();
         borderPane.setTop(commonLayout);        
         BorderPane.setMargin(commonLayout, new Insets(20));
         borderPane.setCenter(textPane);        
@@ -106,110 +103,78 @@ public class Main extends Application
         BorderPane.setMargin(bottomBox, new Insets(10));
         borderPane.getStylesheets().add("abc.css");
 
-        // image panel
-        Image image = new Image("boroughs.png");
-        ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(700);
-        imageView.setFitWidth(780);
-
-        //create new hex shaped buttons
-        GridPane gridPane = new GridPane();
-        /*HexaButton hexButton1 = new HexaButton();
-        HexaButton hexButton2 = new HexaButton();
-        HexaButton hexButton3 = new HexaButton();
-        HexaButton hexButton4 = new HexaButton();
-        HexaButton hexButton5 = new HexaButton();*/
-
-        //add buttons to stackpane 
-        StackPane stackPane = new StackPane();
-        /*stackPane.getChildren().addAll(imageView,
-            hexButton1.getButton("Tower Hill", 72,0), 
-            hexButton2.getButton("Lambeth", -40,190),
-            hexButton3.getButton("Westminster", -40, 0),
-            hexButton4.getButton("Hackney", 140,-100),
-            hexButton5.getButton("Richmond", -300,190));*/
-
         // back and forward button are initially disabled
         backButton.setDisable(true);
-        buttonForward.setDisable(true);
+        forwardButton.setDisable(true);
 
-        buttonForward.setOnAction( e -> {
-                if (counter==0) {
-                    analyzeBoxValues(fromComboBox1, toComboBox1);
-                    if (correctBoxValues) {
-                        borderPane.setCenter(stackPane);
-                        counter++;
-                    }
-                } else if  (counter==1) {
-                    borderPane.setCenter(textPane);
-                    //for testing
-                    //borderPane.setLeft(hex1.getButton());
-                    counter=0;
-                }
-            });
+        forwardButton.setOnAction(this::forwardAction);
+        backButton.setOnAction(this::backAction);;
 
-        backButton.setOnAction( e -> {
-                if (counter==0) {
-                    analyzeBoxValues(fromComboBox1, toComboBox1);
-                    if (correctBoxValues) {
-                        borderPane.setCenter(imageView);
-                        counter++;
-                    }
-                } else if  (counter==1) {
-                    borderPane.setCenter(textPane);
-                    counter=0;
-                }
-            });
+        //ActionEvent for the combo boxes
+        fromComboBox.setOnAction(this::fromAction);
+        toComboBox.setOnAction(this::toAction);
 
-        fromComboBox1.setOnAction( e -> {
-                text1.setText("Welcome to London Property Marketplace \n\n"  +
-                    "Minimum price: " + displayPrice(fromComboBox1) + "\n\n Maximum price: " + displayPrice(toComboBox1));
-                disableFlagA = true; //if both combobox have input back and forward button will be enabled
-                if (disableFlagA && disableFlagB) {
-                    backButton.setDisable(false);
-                    buttonForward.setDisable(false);
-                }
-            });
-
-        toComboBox1.setOnAction( e-> {
-
-                text1.setText("Welcome to London Property Marketplace \n\n" +
-                    "Minimum price: " + displayPrice(fromComboBox1) + "\n\n Maximum price: " + displayPrice(toComboBox1));
-                disableFlagB = true; //if both combobox have input back and forward button will be enabled
-                if (disableFlagA && disableFlagB) {
-                    backButton.setDisable(false);
-                    buttonForward.setDisable(false);
-                }
-            });
-
-        //test code for popup    
-        StackPane popupPane = new StackPane();
-        popupPane.setPrefSize(200, 20);
-        popupPane.setStyle("-fx-background-color: pink;");
-
-        Label popupLabel = new Label("This is a label");
-
-        popupPane.getChildren().add(popupLabel);
-
-        Popup popup = new Popup();
-        popup.getContent().add(popupPane);
-
-        stackPane.hoverProperty().addListener((obs, oldVal, newValue) -> {
-        if (newValue) {
-        Bounds bnds = stackPane.localToScreen(stackPane.getLayoutBounds());
-        double x = bnds.getMinX() - (popupPane.getWidth() / 2) + (stackPane.getWidth() / 2);
-        double y = bnds.getMinY() - popupPane.getHeight();
-        popup.show(stackPane, x, y);
-        } else {
-        popup.hide();
-        }
-        });
-        
-        //end of test code    
+        //lines at top and bottom
+        bottomBox.setStyle("-fx-border-color: black; -fx-border-width: 1px 0px 0px 0px; -fx-padding: 5px ;");
+        commonLayout.setStyle("-fx-border-color: black; -fx-border-width: 0px 0px 1px 0px; -fx-padding: 5px ;");    
 
         // Show the Stage (window)
         window.setScene(new Scene(borderPane,500, 500));
         window.show();
+    }
+
+    /**
+     * Display the image and buttons on the second screen.
+     * @return The StackPane displaying t5he image and buttons.
+     */
+    private StackPane imagePane()
+    {
+        Image image = new Image("boroughs.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(700);
+        imageView.setFitWidth(780);
+        stackPane.getChildren().addAll(imageView); 
+        
+        // GridPane gridPane = new GridPane();
+        // HexButton hexButton1 = new HexButton();
+        // HexButton hexButton2 = new HexButton();
+        // HexButton hexButton3 = new HexButton();
+        // HexButton hexButton4 = new HexButton();
+        // HexButton hexButton5 = new HexButton();
+
+        // //add buttons to stackpane 
+        // StackPane stackPane = new StackPane();
+        // stackPane.getChildren().addAll(imageView, 
+        // hexButton1.getButton("Tower Hill", 72,0), 
+        // hexButton2.getButton("Lambeth", -40,190),
+        // hexButton3.getButton("Westminster", -40, 0),
+        // hexButton4.getButton("Hackney", 140,-100),
+        // hexButton5.getButton("Richmond", -300,190));
+
+        //test code for popup
+        // StackPane popupPane = new StackPane();
+        // popupPane.setPrefSize(200, 20);
+        // popupPane.setStyle("-fx-background-color: pink;");
+
+        // Label popupLabel = new Label("This is a label");
+        // popupPane.getChildren().add(popupLabel);
+
+        // Popup popup = new Popup();
+        // popup.getContent().add(popupPane);
+
+        // stackPane.hoverProperty().addListener((obs, oldVal, newValue) -> {
+        // if (newValue) {
+        // Bounds bnds = stackPane.localToScreen(stackPane.getLayoutBounds());
+        // double x = bnds.getMinX() - (popupPane.getWidth() / 2) + (stackPane.getWidth() / 2);
+        // double y = bnds.getMinY() - popupPane.getHeight();
+        // popup.show(stackPane, x, y);
+        // } else {
+        // popup.hide();
+        // }
+        // });
+        //end of test code
+        
+        return stackPane;
     }
 
     /**
@@ -271,6 +236,68 @@ public class Main extends Application
         }
         else {
             return "<Please select a value>";
+        }
+    }
+
+    /**
+     * Action of the fromComboBox.
+     * @param ActionEvent.
+     */
+    private void fromAction(ActionEvent event)
+    {
+        text1.setText("Welcome to London Property Marketplace \n\n"  +
+            "Minimum price: " + displayPrice(fromComboBox) + "\n\n Maximum price: " + displayPrice(toComboBox));
+        disableFlagA = true; //if both combobox have input back and forward button will be enabled
+        if (disableFlagA && disableFlagB) {
+            backButton.setDisable(false);
+            forwardButton.setDisable(false);
+        }
+    }
+
+    /**
+     * Action of the toComboBox.
+     * @param ActionEvent.
+     */
+    private void toAction(ActionEvent event)
+    {
+        text1.setText("Welcome to London Property Marketplace \n\n" +
+            "Minimum price: " + displayPrice(fromComboBox) + "\n\n Maximum price: " + displayPrice(toComboBox));
+        disableFlagB = true; //if both combobox have input back and forward button will be enabled
+        if (disableFlagA && disableFlagB) {
+            backButton.setDisable(false);
+            forwardButton.setDisable(false);
+        }
+    }
+
+    /**
+     * Action of the backButton.
+     * @param ActionEvent.
+     */
+    private void forwardAction(ActionEvent event)
+    {
+        if (counter==0) {
+            analyzeBoxValues(fromComboBox, toComboBox);
+            if (correctBoxValues) {
+                borderPane.setCenter(imagePane());
+                counter++;
+            }
+        } else if  (counter==1) {
+            borderPane.setCenter(textPane);
+            counter=0;
+        }
+    }
+
+    private void backAction(ActionEvent event)
+    {
+        if (counter==0) {
+            analyzeBoxValues(fromComboBox, toComboBox);
+            if (correctBoxValues) {
+                borderPane.setCenter(imagePane());
+                counter++;
+            }
+        } else if  (counter==1) {
+            borderPane.setCenter(textPane);
+            counter=0;
         }
     }
 
