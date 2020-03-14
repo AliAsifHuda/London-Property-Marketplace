f034453e49212268f5ac339fd5455000a9b91957
Ali Asif <k19033243@kcl.ac.uk>
2020-03-14 23:21:02 +0000

    button size was set to coreect dimensions

--- HexButton.java
+++ HexButton.java
@@ -62,7 +62,7 @@ public class HexButton
                 200.0, -250.0
         );
         myButton.setShape(polygon);
-        myButton.setMinSize(110,110);
+        myButton.setMaxSize(110,110);
         myButton.setText(boroughName);
     }
 
